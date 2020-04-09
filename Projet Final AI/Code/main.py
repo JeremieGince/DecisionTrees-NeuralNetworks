3ebeffc6307c9441c55c057e1cef7f9d29d7ade3
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-04-09 12:46:19 -0400

    Update main.py

--- Projet Final AI/Code/main.py
+++ Projet Final AI/Code/main.py
@@ -6,4 +6,3 @@ import NeuralNet  # importer la classe du Réseau de Neurones
 import DecisionTree  # importer la classe de l'Arbre de Décision
 # importer d'autres fichiers et classes si vous en avez développés
 # importer d'autres bibliothèques au besoin, sauf celles qui font du machine learning
-
